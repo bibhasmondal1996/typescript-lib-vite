5147e4618cd4874f33c4cd5e12b1c3e8f20a193e
Bibhas <bibhasmondal96@msn.com>
2022-11-27 08:47:53 +0530

    type configured

---
+++ dist/types/components/MyButton.vue.d.ts
@@ -0,0 +1,16 @@
+declare const _default: import("vue").DefineComponent<{
+    primary: {
+        type: import("vue").PropType<boolean>;
+    } & {
+        default: boolean;
+    };
+}, () => void, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    primary: {
+        type: import("vue").PropType<boolean>;
+    } & {
+        default: boolean;
+    };
+}>>, {
+    primary: boolean;
+}>;
+export default _default;
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { MyButton } from 'my-component-lib';
-import '/node_modules/my-component-lib/dist/style.css';
+// import { MyButton } from 'my-component-lib';
+// import '/node_modules/my-component-lib/dist/style.css';
 </script>
 
 <template>
